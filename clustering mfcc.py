7bdc9d59d946b6514c33a0f31d0e74c04262d890
pixidust <bioy.marianne@gmail.com>
2016-02-02 18:51:36 +0100

    mfcc

---
+++ clustering mfcc.py
@@ -0,0 +1,95 @@
+
+import numpy
+import matplotlib.pyplot as plt
+import operator
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import numpy as np
+import math
+import preprocess
+
+
+
+# Read mat file and align file.
+filename = '/home/marianne/Developpement/neuroPrononciation/ '
+alignfile = '/home/marianne/Developpement/Bref80_L4M01.aligned'
+fbank = numpy.load(filename)
+
+#parametres = mfcc("/home/marianne/Developpement/Bref80_L4M01.wav" , 0.02 , 0.01 , 40)
+
+
+pho = preprocess.create_reference(fbank,alignfile)
+
+
+#cluster the data to n_clusters class.
+n_clusters = 10
+kmeans = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
+kmeans.fit(fbank)
+
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
+
+#Calculate the number of each phoneme in each cluster
+cluster_pho = [None]*n_clusters
+for label,ph in zip(labels,pho):
+    if(cluster_pho[label-1]==None):
+        cluster_pho[label-1]= {ph:1}
+    elif ph in cluster_pho[label-1].keys():
+        cluster_pho[label-1][ph]+=1
+    else:
+        cluster_pho[label-1][ph]=1
+cluster_pho[:]= [sorted(x.items(),key = operator.itemgetter(1),reverse=True)for x in cluster_pho]
+
+# use bar chart to visualize each class
+nrows = int(round(math.sqrt(n_clusters)))
+ncols = int(math.ceil(n_clusters/round(math.sqrt(n_clusters))))
+figures,axs  = plt.subplots(nrows = nrows,ncols = ncols)
+
+for ax,data in zip(axs.ravel(),cluster_pho):
+    data = zip(*data)
+    ax.bar(range(len(data[0])),data[1],width = 0.2)
+    ax.set_xticks(np.arange(len(data[0]))+0.1)
+    ax.set_xticklabels(data[0],rotation = 0)
+plt.show()
+
+#print(cluster_pho)
+
+
+#Visulization by PCA
+reduced_data = PCA(n_components=2).fit_transform(fbank)
+kmeans = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
+kmeans.fit(reduced_data)
+
+# Step size of the mesh. Decrease to increase the quality of the VQ.
+h = .02     # point in the mesh [x_min, m_max]x[y_min, y_max].
+
+# Plot the decision boundary. For that, we will assign a color to each
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# Obtain labels for each point in mesh. Use last trained model.
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,
+           aspect='auto', origin='lower')
+
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+# Plot the centroids as a white X
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+          'Centroids are marked with white cross')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
---
+++ mfcc.py
@@ -0,0 +1,58 @@
+import librosa
+import math
+import numpy
+from numpy import shape
+
+##############################################
+#genere les coefficients cepstraux du fichier son, en utilisant une fenetre glissante
+#parametres:
+##path(chaine de caracteres) : chemin du fichier son sur la machine utilisateur
+##taille_fenetre(secondes) : taille de la fenetre glissante : extraction des parametres pour les fenetres de cette taille
+##overlapping(secondes) : deplacement de la fenetre
+##nb_mel(int) : nombre de coefficients a generer
+#retour:matrice nb_fenetres*nb_mel : les coefficients pour chaque fenetre
+##############################################
+def mfcc(path, taille_fenetre, overlapping, nb_mel):
+    '''
+    :genere les coefficients cepstraux du fichier son, en utilisant une fenetre glissante
+    :param path: (string) chemin du fichier son sur la machine utilisateur
+    :param taille_fenetre: (secondes) taille de la fenetre glissante : extraction des parametres pour les fenetres de cette taille
+    :param overlapping: (secondes)  deplacement de la fenetre
+    :param nb_mel: (int) nombre de coefficients a generer
+    :return: matrice (liste de tableaux) nb_fenetres*nb_mel : les coefficients pour chaque fenetre
+    '''
+
+    #acquisition du signal avec le taux d'echantillonage par defaut (22050)
+    son, sr  = librosa.core.load(path)
+    duree = librosa.core.get_duration(son)
+
+    #exceptions sur les parametres de la fonction
+    if taille_fenetre>duree:
+        raise Exception
+    if overlapping>duree:
+        raise Exception
+
+    #calcul du nombre de fenetres total
+    nb_ech_fenetre = sr*taille_fenetre
+    nb_fenetres = int(math.floor(duree/overlapping))
+    if (nb_fenetres-1)*overlapping+taille_fenetre > duree:
+        nb_fenetres = nb_fenetres-1
+
+    #pour chacune des fenetres, calcul et stockage de la mfcc
+    son_mfcc = [None]*nb_fenetres
+    for i in range(nb_fenetres):
+        son_inter = son[i*overlapping:nb_ech_fenetre]
+        son_inter2 = librosa.feature.mfcc(son_inter,sr,None,nb_mel)
+        son_mfcc[i] = son_inter2.T[0]
+    #print shape(son_mfcc)
+    #print nb_fenetres, nb_mel
+    #print son_mfcc
+
+    #enregistrement de la matrice dans un fichier csv
+    a = numpy.asarray(son_mfcc)
+    numpy.savetxt("mfcc.mat" , a )
+    return son_mfcc
+
+
+piou = mfcc("/home/marianne/Developpement/Bref80_L4M01.wav" , 0.02 , 0.01 , 40)
+#print piou
