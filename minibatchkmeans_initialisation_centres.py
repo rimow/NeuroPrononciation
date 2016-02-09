dcc5fea9e873d2a6f06da5c1b33689cd5f910baf
rimow <rim.echchouqi.re@gmail.com>
2016-02-09 16:01:11 +0100

    Update minibatchkmeans_initialisation_centres.py

--- minibatchkmeans_initialisation_centres.py
+++ minibatchkmeans_initialisation_centres.py
@@ -147,5 +147,5 @@ def minibatchkmeans_avec_initialisation_centres(X, n_clusters, path_classement, 
 signal, sampling_rate = librosa.load('1.wav') #load du fichier audio
 fband = FourierTransform('1.wav', int(0.02*sampling_rate), int(0.01*sampling_rate))
 alignfile = '11.aligned' # le fichier contenant les annotations expert
-minibatchkmeans_avec_initialisation_centres(fband, 6, 'classement1', alignfile,0)
+minibatchkmeans_avec_initialisation_centres(fband.transpose(), 6, 'classement1', alignfile,0)
 
