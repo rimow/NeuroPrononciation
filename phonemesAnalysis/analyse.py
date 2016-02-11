f798f382e8671faecfe9c86cf7a97d04940001ea
pixidust <bioy.marianne@gmail.com>
2016-02-11 10:43:12 +0100

    ajout option enregistrement des resultats

--- phonemesAnalysis/analyse.py
+++ phonemesAnalysis/analyse.py
@@ -6,13 +6,14 @@ import operator
 from fonctions_utiles import getPhonemeDict
 
 
-def pourcentage(Y , n_clusters , labels , dict_path , type_separation):
+def pourcentage(Y , n_clusters , labels , dict_path , type_separation, fichier = None):
     '''
     :param Y: veceteur contenant les phonemes correpondant a chaque vecteur (taille n_vectors)
     :param n_clusters: nombre de clusters: doit matcher avec type separation : 3 classes pour voise et consonnes, plus pour fricatives
     :param labels: tableau resultat du clustering
     :param dict_path: chemin du classement des phonemes selon leur caracteristiques (consonne = 0, voise = 1, fricative... = 2)
     :param type_separation: type de phonemes discrimines : voises. consonnes? fricatives/occlusives? (voir data/classement et data/detail_classement)
+    :param fichier: si on souhaite enregistrer les resultats dans un fichier csv, on precise le chemin sinon on ne met rien
     :return:
     '''
 
@@ -20,18 +21,20 @@ def pourcentage(Y , n_clusters , labels , dict_path , type_separation):
     Y_v_non_v = getY_v_non_v(Y , dict , type_separation)
 
     if type_separation == 0:
-        printRatiosConsonnes(n_clusters , labels , Y_v_non_v)
+        printRatiosConsonnes(n_clusters , labels , Y_v_non_v, fichier)
     elif type_separation == 1:
-        printRatiosVoise(n_clusters , labels , Y_v_non_v)
+        printRatiosVoise(n_clusters , labels , Y_v_non_v, fichier)
     else:
-        printRatiosCategories(n_clusters , labels , Y_v_non_v)
+        printRatiosCategories(n_clusters , labels , Y_v_non_v, fichier)
 
 
-def printRatiosConsonnes(nb_classes , Y_cluster , y_cons_voy):
+def printRatiosConsonnes(nb_classes , Y_cluster , y_cons_voy, fichier = None):
     """
     :param nb_classes: le nombre de classes resultant du clustering
     :param Y_cluster: tableau contenat les classes attribuees a chaque fenetre de X (taille n_vectors)
     :param y_voise_non_voise: vecteur contenant les classes : 0 pour non voise, 1 pour voise, 2 pour silence (taile n_vectors)
+    :param type_separation: type de phonemes discrimines : voises. consonnes? fricatives/occlusives? (voir data/classement et data/detail_classement)
+    :param fichier: si on souhaite enregistrer les resultats dans un fichier csv, on precise le chemin sinon on ne met rien
     :return: Pour chacune des classes, ecris le pourcentage de voyelles, consonnes et silences, et le pourcentage de
             voises, non voises et silences (pourcentage du total des phoneme)
   """
@@ -47,19 +50,30 @@ def printRatiosConsonnes(nb_classes , Y_cluster , y_cons_voy):
     nb_voyelles = len([i for i in y_cons_voy if i == 1])
     nb_n_consonnes = len([i for i in y_cons_voy if i == 0])
     print 'Voyelles et consonnes (en pourcentage de chacune des classes):'
+    valeurs = []
     for cl in range(nb_classes):
         p1 = 100. * len([i for i in y_cons_voy[classes[cl]] if i == 0]) / nb_n_consonnes  # % classe cl et consonne
         p2 = 100. * len([i for i in y_cons_voy[classes[cl]] if i == 1]) / nb_voyelles  # % classe cl et voyelle
         p3 = 100. * len([i for i in y_cons_voy[classes[cl]] if i == 2]) / nb_silences  # % classe cl et silence
         print '   Classe ' , cl , ':'
         print '     Consonnes :' , p1 , '\n     Voyelles :' , p2 , '\n     Silences :' , p3 , '\n'
+        ligne = [int(cl), round(p1), round(p2),round(p3)]
+        valeurs.append(ligne)
 
+    if fichier != None:
+        f = open(fichier, "a")
+        f.write("classes,consonnes,voyelles,silences\n")
+        tableau = np.asarray(valeurs, dtype=np.float64)
+        np.savetxt(f, tableau, delimiter =',')
+        f.close()
 
-def printRatiosVoise(nb_classes , Y_cluster , y_voise_non_voise):
+def printRatiosVoise(nb_classes , Y_cluster , y_voise_non_voise, fichier = None):
     """
     :param nb_classes: le nombre de classes resultant du clustering
     :param Y_cluster: tableau (taille n_vectors) contenant les classes attribuees a chaque fenetre de X (taille n_vectors x n_parametres)
     :param y_voise_non_voise: tableau (taille n_vectors) contenant les classes : 0 pour non voise, 1 pour voise, 2 pour silence
+    :param type_separation: type de phonemes discrimines : voises. consonnes? fricatives/occlusives? (voir data/classement et data/detail_classement)
+    :param fichier: si on souhaite enregistrer les resultats dans un fichier csv, on precise le chemin sinon on ne met rien
     :return: Pour chacune des classes, ecris le pourcentage de voyelles, consonnes et silences, et le pourcentage de
             voises, non voises et silences (pourcentage du total des phoneme)
   """
@@ -75,19 +89,31 @@ def printRatiosVoise(nb_classes , Y_cluster , y_voise_non_voise):
     nb_voises = len([i for i in y_voise_non_voise if i == 1])
     nb_n_voises = len([i for i in y_voise_non_voise if i == 0])
     print 'Pourcentage des voises, non voises et silences :'
+    valeurs = []
     for cl in range(nb_classes):
         p1 = 100. * len([i for i in y_voise_non_voise[classes[cl]] if i == 0]) / nb_n_voises  # % classe cl et non voise
         p2 = 100. * len([i for i in y_voise_non_voise[classes[cl]] if i == 1]) / nb_voises  # % classe cl et voise
         p3 = 100. * len([i for i in y_voise_non_voise[classes[cl]] if i == 2]) / nb_silences  # % classe cl et silence
         print '   Classe ' , cl , ':'
         print '     Non voises :' , p1 , '\n    Voises :' , p2 , '\n   Silences :' , p3 , '\n'
+        ligne = [int(cl), round(p1), round(p2),round(p3)]
+        valeurs.append(ligne)
+
+    if fichier != None:
+        f = open(fichier, "a")
+        f.write("classes,non-voisees,voisees,silences\n")
+        tableau = np.asarray(valeurs, dtype=np.float64)
+        np.savetxt(f, tableau, delimiter =',')
+        f.close()
 
 
-def printRatiosCategories(nb_classes , Y_cluster , y_categorie):
+def printRatiosCategories(nb_classes , Y_cluster , y_categorie, fichier = None):
     """
     :param nb_classes: le nombre de classes resultant du clustering
     :param Y_cluster: tableau (taille n_vectors) contenant les classes attribuees a chaque fenetre de X (taille n_vectors x n_parametres)
     :param y_categorie: tableau (taille n_vectors) contenant les classes : 0 occlusive, 1 fricative ...
+    :param type_separation: type de phonemes discrimines : voises. consonnes? fricatives/occlusives? (voir data/classement et data/detail_classement)
+    :param fichier: si on souhaite enregistrer les resultats dans un fichier csv, on precise le chemin sinon on ne met rien
     :return: Pour chacune des classes, ecris le pourcentage de fricatives, occlusives ... dans chaque classe
   """
     # Au cas ou :
@@ -105,7 +131,7 @@ def printRatiosCategories(nb_classes , Y_cluster , y_categorie):
     nb_voyelle = len([i for i in y_categorie if i == 3])
     nb_semi_consone = len([i for i in y_categorie if i == 4])
     nb_silence = len([i for i in y_categorie if i == 5])
-
+    valeurs = []
     for cl in range(nb_classes):
         p1 = 100. * len([i for i in y_categorie[classes[cl]] if i == 0]) / nb_occlusives  # % classe cl et non voise
         p2 = 100. * len([i for i in y_categorie[classes[cl]] if i == 1]) / nb_fricative  # % classe cl et voise
@@ -116,6 +142,14 @@ def printRatiosCategories(nb_classes , Y_cluster , y_categorie):
 
         print '   Classe ' , cl , ':'
         print '     Occlusives :' , p1 , '\n    Fricatives :' , p2 , '\n   Nasale :' , p3 , '\n  Voyelle :' , p4 , '\n  Semi_consonne :' , p5 , '\n  Silences :' , p6 , '\n'
+        valeurs.append([int(cl), round(p1), round(p2),round(p3), round(p4), round(p5),round(p6)])
+
+    if fichier !=None:
+        f = open(fichier, "a")
+        f.write("classes,occlusives,fricatives,nasales,voyelles,semi-consonnes,silences\n")
+        tableau = np.asarray(valeurs)
+        np.savetxt(f, tableau, delimiter =',')
+        f.close()
 
 
 def getY_v_non_v(Y , dict , type_separation):
