84b2b1a5a7b7e9bce699f716e96fb48c48650653
samuelN7 <samuel.guery.85@gmail.com>
2016-02-18 16:58:40 +0100

    lda plus complet

---
+++ reseau/classificationLDA/LDA.py
@@ -0,0 +1,53 @@
+import pickle
+import numpy as np
+from utiles_classification import *
+from supprimerCartesVides import *
+
+#Files
+mapconv1J_file='maps/PHONIM_l_conv1_35maps_th0.001000.pkl'
+mapconv2J_file='maps/PHONIM_l_conv2_35maps_th0.001000.pkl'
+mapmp2J_file='maps/PHONIM_l_mp2_35maps_th0.001000.pkl'
+denseJ_file = 'maps/PHONIM_l_dense1_35maps_th0.001000.pkl'
+mapconv1F_file='maps/BREF80_l_conv1_35maps_th0.500000.pkl'
+mapconv2F_file='maps/BREF80_l_conv2_35maps_th0.500000.pkl'
+mapmp2F_file='maps/BREF80_l_mp2_35maps_th0.500000.pkl'
+denseF_file = 'maps/BREF80_l_dense1_35maps_th0.500000.pkl'
+
+#Load data
+#conv1J = load_maps(mapconv1J_file)
+# conv2J = load_maps(mapconv2J_file)
+mpJ = load_maps(mapmp2J_file)
+# denseJ = load_maps(denseJ_file)
+#conv1F = load_maps(mapconv1F_file)
+# conv2F = load_maps(mapconv2F_file)
+mpF = load_maps(mapmp2F_file)
+# denseF = load_maps(denseF_file)
+
+# liste des ensembles de dictionnaires que l'on veut prendre en donnees d'entree
+all_liste_dics = [[mpF],[mpJ],[mpJ,mpF]]
+labels_dictionnaire = ['FR','JAP','FR JAP'] #Pour ecrire dans le fichier
+
+# Listes des phonemes que l'on veut traiter
+all_listes_phonemes = [['R'],['v'],['R','v']]
+labels_phonemes = [' R ',' v ',' R v '] #Pour ecrire dans le fichier
+
+#Categories que l'on veut traiter, on prend tout en general
+all_categories = mpJ.keys()
+
+#Type de classification que l'on veut faire
+types = ['c_inc','r_v','fr_jap']
+
+a_ignorer = []
+l_cartes = []
+n_folds = 5
+f_res = open('LDA_resultats_complet_mp22.txt', 'w')
+
+
+for i,dics in enumerate(all_liste_dics):
+    for j,ph in enumerate(all_listes_phonemes):
+        for type in types:
+            f_res.write('Dictionnaires :'+labels_dictionnaire[i]+' Phonemes :'+labels_phonemes[j]+' Type de classification :'+type+'\n')
+            score1,score2 = ldaClassification(dics,liste_phonemes=ph,liste_categories=all_categories,num_cartes=l_cartes,a_ignorer=a_ignorer,n_folds=n_folds,type=type)
+            f_res.write('Score de la classification, une donnee=une carte : '+str(score2)+'\nScore de la classification, une donnee=un ensemble de cartes : '+str(score1)+'\n \n')
+
+f_res.close()
