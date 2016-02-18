27aff5b656d19e973f9a7ca303c8426d1f71e023
samuelN7 <samuel.guery.85@gmail.com>
2016-02-18 17:05:37 +0100

    rangement

---
+++ reseau/classificationLDA/classificationLDAunitaire.py
@@ -0,0 +1,24 @@
+import pickle
+import numpy as np
+from utiles_classification import *
+from supprimerCartesVides import *
+from utils_maps import *
+#Files
+mapconv1J_file='maps/PHONIM_l_conv1_35maps_th0.001000.pkl'
+mapconv1F_file='maps/BREF80_l_conv1_35maps_th0.500000.pkl'
+
+#Load data
+conv1J = load_maps(mapconv1J_file)
+conv1F = load_maps(mapconv1F_file)
+ph = ['R']
+dics = [conv1J]
+cat = conv1J.keys()
+l_cartes = [25, 56, 114, 120]
+a_ignorer = []
+n_folds = 5
+type = 'c_inc'
+
+ldaClassification(dics,ph,cat,l_cartes,a_ignorer,n_folds,type)
+
+#good = goodmaps([],seuil=3)
+#print good
--- reseau/classificationLDA/utiles_classification.py
+++ reseau/classificationLDA/utiles_classification.py
@@ -16,11 +16,13 @@ def getData_dense(liste_dictionnaires = [], liste_categories = [], liste_phoneme
     :param liste_dictionnaires: liste de dictionnaires de vecteurs denses
     :param liste_categories: liste de categories
     :param liste_phonemes:  liste de phonemes
-    :return: une matrice de parametre X ((nb_dic*nb_cat*nb_ph*nb_cartes)*taille_vecteur), et un vecteur de parametre Y (nb_dic*nb_cat*nb_ph*nb_cartes)
+    :return: une matrice de parametre X ((nb_dic*nb_cat*nb_ph*nb_cartes)*taille_vecteur), et des vecteurs de parametre Y (nb_dic*nb_cat*nb_ph*nb_cartes)
+    differenciant les corrects des incorrects, les phonemes, les langues
  """
  X = []
  Y_c_inc = []
  Y_r_v = []
+ Y_fr_jap = []
  for idic,dic in enumerate(liste_dictionnaires):
      for icat,cat in enumerate(liste_categories):
          if icat==0 or icat==1:
@@ -28,27 +30,32 @@ def getData_dense(liste_dictionnaires = [], liste_categories = [], liste_phoneme
          else:
              classe_c_inc=0
          for iph,ph in enumerate(liste_phonemes):
-             if ph=='R':
-                 classe_r_v = 1
-             else:
-                 classe_r_v = 0
+             # if ph=='R':
+             #     classe_r_v = 1
+             # else:
+             #     classe_r_v = 0
              for exemplaire in range(np.array(dic[cat][ph]).shape[0]):
                X.append(dic[cat][ph][exemplaire])
                Y_c_inc.append(classe_c_inc)
-               Y_r_v.append(classe_r_v)
- return np.array(X), np.array(Y_c_inc),np.array(Y_r_v)
+               Y_r_v.append(iph)
+               Y_fr_jap.append(idic)
+ return np.array(X), np.array(Y_c_inc),np.array(Y_r_v), np.array(Y_fr_jap)
 
-def getData_maps(liste_dictionnaires = [], liste_categories = [], liste_phonemes = [], a_ignorer=[]):
+def getData_maps(liste_dictionnaires = [], liste_categories = [], liste_phonemes = [], a_ignorer=[],liste_cartes=[]):
  """
     :param liste_dictionnaires: liste de dictionnaires de cartes
     :param liste_categories: liste de categories
     :param liste_phonemes: liste de phonemes
-    :return: une matrice de parametre X ((nb_dic*nb_cat*nb_ph*nb_cartes)*nb_elts_carte), et un vecteur de parametre Y (nb_dic*nb_cat*nb_ph*nb_cartes)
- """
+    :param a_ignorer: cartes que l'on veut ignorer
+    :param liste_cartes : cartes que l'on considere
+    :return: une matrice de parametre X ((nb_dic*nb_cat*nb_ph*nb_cartes)*taille_vecteur), et des vecteurs de parametre Y (nb_dic*nb_cat*nb_ph*nb_cartes)
+    differenciant les corrects des incorrects, les phonemes, les langues """
  X = []
  Y_c_inc = []
  Y_r_v = []
-
+ Y_fr_jap = []
+ if liste_cartes==[]:
+     liste_cartes=range(np.array(liste_dictionnaires[0][liste_categories[0]][liste_phonemes[0]]).shape[1])
  for idic,dic in enumerate(liste_dictionnaires):
      for icat,cat in enumerate(liste_categories):
          if icat==0 or icat==1:
@@ -56,29 +63,35 @@ def getData_maps(liste_dictionnaires = [], liste_categories = [], liste_phonemes
          else:
              classe_c_inc=0
          for iph,ph in enumerate(liste_phonemes):
-             if ph=='R':
-                 classe_r_v = 1
-             else:
-                 classe_r_v = 0
+             # if ph=='R':
+             #     classe_r_v = 1
+             # else:
+             #     classe_r_v = 0
              for exemplaire in range(np.array(dic[cat][ph]).shape[0]):
-                 for map in range(np.array(dic[cat][ph]).shape[1]):
+                 for map in liste_cartes:
                     if not(map in a_ignorer):
                       X.append(np.array(dic[cat][ph][exemplaire][map]).flatten())
                       Y_c_inc.append(classe_c_inc)
-                      Y_r_v.append(classe_r_v)
- return np.array(X), np.array(Y_c_inc), np.array(Y_r_v)
+                      Y_r_v.append(iph)
+                      Y_fr_jap.append(idic)
+ return np.array(X), np.array(Y_c_inc), np.array(Y_r_v), np.array(Y_fr_jap)
 
-def getData_onePerMap(liste_dictionnaires = [], liste_categories = [], liste_phonemes = [],a_ignorer=[]):
+def getData_onePerMap(liste_dictionnaires = [], liste_categories = [], liste_phonemes = [],a_ignorer=[],liste_cartes=[]):
  """
 
     :param liste_dictionnaires: liste de dictionnaires de cartes
     :param liste_categories: liste de categories
     :param liste_phonemes: liste de phonemes
-    :return: une matrice de parametre X ((nb_dic*nb_cat*nb_ph)*(nb_cartes*nb_elts_carte), et un vecteur de parametre Y (nb_dic*nb_cat*nb_ph)
- """
+    :param a_ignorer: cartes que l'on veut ignorer
+    :param liste_cartes: cartes que l'on considere
+    :return: une matrice de parametre X ((nb_dic*nb_cat*nb_ph*nb_cartes)*taille_vecteur), et des vecteurs de parametre Y (nb_dic*nb_cat*nb_ph*nb_cartes)
+    differenciant les corrects des incorrects, les phonemes, les langues """
  X = []
  Y_c_inc = []
  Y_r_v = []
+ Y_fr_jap = []
+ if liste_cartes==[]:
+     liste_cartes=range(np.array(liste_dictionnaires[0][liste_categories[0]][liste_phonemes[0]]).shape[1])
  for idic,dic in enumerate(liste_dictionnaires):
      for icat,cat in enumerate(liste_categories):
          if icat==0 or icat==1:
@@ -86,20 +99,22 @@ def getData_onePerMap(liste_dictionnaires = [], liste_categories = [], liste_pho
          else:
              classe_c_inc=0
          for iph,ph in enumerate(liste_phonemes):
-             if ph=='R':
-                 classe_r_v = 1
-             else:
-                 classe_r_v = 0
+             # if ph=='R':
+             #     classe_r_v = 1
+             # else:
+             #     classe_r_v = 0
              for exemplaire in range(np.array(dic[cat][ph]).shape[0]):
                  inter = []
-                 for map in range(np.array(dic[cat][ph]).shape[1]):
+                 for map in liste_cartes:
                     if not(map in a_ignorer):
                       inter.append(np.array(dic[cat][ph][exemplaire][map]).flatten())
                  if not(inter==[]):
                    X.append(np.concatenate(np.array(inter)))
                    Y_c_inc.append(classe_c_inc)
-                   Y_r_v.append(classe_r_v)
- return np.array(X), np.array(Y_c_inc), np.array(Y_r_v)
+                   Y_r_v.append(iph)
+                   Y_fr_jap.append(idic)
+
+ return np.array(X), np.array(Y_c_inc), np.array(Y_r_v), np.array(Y_fr_jap)
 
 def LDAmeanScore(X,Y,n_folds):
  """
@@ -128,3 +143,59 @@ def LDAmeanScore(X,Y,n_folds):
     return 100.*np.mean(np.array(scores))
  else:
       return -1
+
+
+def ldaClassification(liste_dictionnaires,liste_phonemes,liste_categories,num_cartes=[],a_ignorer=[],n_folds=5,type='c_inc'):
+ """
+ :param liste_dictionnaires: liste de dictionnaires
+ :param liste_phonemes: liste de phonemes
+ :param liste_categories: liste des categories
+ :param num_cartes: numeros des cartes interessantes (si vide, on prend tout)
+ :param a_ignorer: numeros des cartes que l'on ignore
+ :param n_folds: n_folds pour le kfold
+ :param type: type de classification, c_inc pour correct/incorrect, fr_jap pour francais/japonais, r_v pour les phonemes r et v
+ :return: deux scores moyens de classification, obtenus avec deux types de donnees ( getData_onePerMap, et getData_maps)
+ """
+
+ print 'getData_OnePerMap : '
+ X,Y_c_inc,Y_r_v, Y_fr_jap = getData_onePerMap(liste_dictionnaires=liste_dictionnaires,liste_categories=liste_categories,liste_phonemes=liste_phonemes,liste_cartes=num_cartes,a_ignorer=a_ignorer)
+ if type=='r_v':
+     Y=Y_r_v
+ elif type=='c_inc':
+     Y=Y_c_inc
+ elif type=='fr_jap':
+     Y=Y_fr_jap
+ else:
+     Y=Y_c_inc
+ score1 = LDAmeanScore(X,Y,n_folds)
+ # Verification des dimensions
+ Xsh = X.shape
+ convSh = np.array(liste_dictionnaires[0]['correct_OK']['v']).shape
+ print 'Verification des dimensions : '
+ print 'X : ',Xsh
+ print 'getData_onePerMap : ',len(liste_dictionnaires)*len(liste_phonemes)*len(liste_categories)*convSh[0]
+
+ print '\ngetData_maps :'
+ X,Y_c_inc,Y_r_v, Y_fr_jap = getData_maps(liste_dictionnaires=liste_dictionnaires,liste_categories=liste_categories,liste_phonemes=liste_phonemes,liste_cartes=num_cartes,a_ignorer=a_ignorer)
+ if type=='r_v':
+     Y=Y_r_v
+ elif type=='c_inc':
+     Y=Y_c_inc
+ elif type=='fr_jap':
+     Y=Y_fr_jap
+ else:
+     Y=Y_c_inc
+ score2 = LDAmeanScore(X,Y,n_folds)
+ # Verification des dimensions
+ Xsh = X.shape
+ convSh = np.array(liste_dictionnaires[0]['correct_OK']['v']).shape
+ print 'Verification des dimensions : '
+ print 'X : ',Xsh
+ if len(num_cartes)==0:
+     nb_cartes = convSh[1]
+ else:
+     nb_cartes = len(num_cartes)
+ print 'getData_maps : ',len(liste_dictionnaires)*len(liste_phonemes)*len(liste_categories)*convSh[0]*nb_cartes
+ print '\n'
+
+ return score1,score2
