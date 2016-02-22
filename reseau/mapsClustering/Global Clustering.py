24fe4a298d872692d2918cab298d6e1f0e717988
samia88 <samia.aalaou@gmail.com>
2016-02-22 11:53:41 +0100

    Update Global Clustering.py

--- reseau/mapsClustering/Global Clustering.py
+++ reseau/mapsClustering/Global Clustering.py
@@ -1,6 +1,10 @@
 from mapsClustering.GenerationClustering import GenerationClustering
+from mapsAnalysis.utiles import imagesCartesInteressantes
 
 GenerationClustering(couche = "conv1", seuilSuppression = 559, seuilBonClustering = 30, fichier = True)
 GenerationClustering(couche = "conv2", seuilSuppression = 559, seuilBonClustering = 30, fichier = True)
 GenerationClustering(couche = "mp2", seuilSuppression = 559, seuilBonClustering = 30, fichier = True)
 GenerationClustering(couche = "dense1", seuilSuppression = 559, seuilBonClustering = 30, fichier = True)
+
+#Example of a call to imagesCartesInteressantes
+imagesCartesInteressantes(5, 6, couche='conv1', clustering=1)
