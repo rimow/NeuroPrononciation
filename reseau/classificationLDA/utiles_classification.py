ca0bf1b7fd48ad450381acc9a8878a831e2bd979
samuelN7 <samuel.guery.85@gmail.com>
2016-02-22 16:38:13 +0100

    modif utiles_classification

--- reseau/classificationLDA/utiles_classification.py
+++ reseau/classificationLDA/utiles_classification.py
@@ -116,6 +116,96 @@ def getData_onePerMap(liste_dictionnaires = [], liste_categories = [], liste_pho
 
  return np.array(X), np.array(Y_c_inc), np.array(Y_r_v), np.array(Y_fr_jap)
 
+def LDAmeanScore(X,Y,n_folds,dim_reduction=0):
+ """
+    :param X: matrice d'entree du classifieur, n_samples*n_parameters
+    :param Y: matrice des labels, n_samples
+    :param n_folds: nombre de tests pour le KFold
+    :return: le score moyen de la validation croisee, affiche ce score
+ """
+ if dim_reduction!=0 and X.shape[1]>dim_reduction:
+     X = dim_reduction_PCA(X,dim_reduction)
+ if (X.shape[0]>n_folds):
+    # Cross validation pour estimer la performance d'un classifieur LDA
+    kf = KFold(n=len(Y), n_folds=n_folds, shuffle=False,
+                               random_state=None)
+    scores = []
+    for train_index, test_index in kf:
+      X_train, X_test = X[train_index,:],X[test_index,:]
+      Y_train, Y_test = Y[train_index],Y[test_index]
+      cl = LDA()
+      # if dim_reduction==0:
+      #   cl = LDA()
+      # else:
+      #   cl =LDA(n_components=dim_reduction)
+      #cl = SVC()
+      #cl = GaussianNB()
+      #cl = KNeighborsClassifier(3)
+      cl.fit(X_train,Y_train)
+      scores.append(cl.score(X_test,Y_test))
+
+
+    print 'Score moyen : ',np.mean(np.array(scores))
+    return 100.*np.mean(np.array(scores))
+ else:
+      return -1
+
+
+def ldaClassification(liste_dictionnaires,liste_phonemes,liste_categories,num_cartes=[],a_ignorer=[],n_folds=5,type='c_inc',dim_reduction=0):
+ """
+ :param liste_dictionnaires: liste de dictionnaires
+ :param liste_phonemes: liste de phonemes
+ :param liste_categories: liste des categories
+ :param num_cartes: numeros des cartes interessantes (si vide, on prend tout)
+ :param a_ignorer: numeros des cartes que l'on ignore
+ :param n_folds: n_folds pour le kfold
+ :param type: type de classification, c_inc pour correct/incorrect, fr_jap pour francais/japonais, r_v pour les phonemes r et v
+ :return: deux scores moyens de classification, obtenus avec deux types de donnees ( getData_onePerMap, et getData_maps)
+ """
+
+ print 'getData_OnePerMap : '
+ X,Y_c_inc,Y_r_v, Y_fr_jap = getData_onePerMap(liste_dictionnaires=liste_dictionnaires,liste_categories=liste_categories,liste_phonemes=liste_phonemes,liste_cartes=num_cartes,a_ignorer=a_ignorer)
+ if type=='r_v':
+     Y=Y_r_v
+ elif type=='c_inc':
+     Y=Y_c_inc
+ elif type=='fr_jap':
+     Y=Y_fr_jap
+ else:
+     Y=Y_c_inc
+ score1 = LDAmeanScore(X,Y,n_folds,dim_reduction)
+ # Verification des dimensions
+ Xsh = X.shape
+ convSh = np.array(liste_dictionnaires[0]['correct_OK']['v']).shape
+ print 'Verification des dimensions : '
+ print 'X : ',Xsh
+ print 'getData_onePerMap : ',len(liste_dictionnaires)*len(liste_phonemes)*len(liste_categories)*convSh[0]
+
+ print '\ngetData_maps :'
+ X,Y_c_inc,Y_r_v, Y_fr_jap = getData_maps(liste_dictionnaires=liste_dictionnaires,liste_categories=liste_categories,liste_phonemes=liste_phonemes,liste_cartes=num_cartes,a_ignorer=a_ignorer)
+ if type=='r_v':
+     Y=Y_r_v
+ elif type=='c_inc':
+     Y=Y_c_inc
+ elif type=='fr_jap':
+     Y=Y_fr_jap
+ else:
+     Y=Y_c_inc
+ score2 = LDAmeanScore(X,Y,n_folds,dim_reduction)
+ # Verification des dimensions
+ Xsh = X.shape
+ convSh = np.array(liste_dictionnaires[0]['correct_OK']['v']).shape
+ print 'Verification des dimensions : '
+ print 'X : ',Xsh
+ if len(num_cartes)==0:
+     nb_cartes = convSh[1]
+ else:
+     nb_cartes = len(num_cartes)
+ print 'getData_maps : ',len(liste_dictionnaires)*len(liste_phonemes)*len(liste_categories)*convSh[0]*nb_cartes
+ print '\n'
+
+ return score1,score2
+
 
 def getData_goodmaps(liste_dictionnaires = [], liste_categories = [], liste_phonemes = [],liste_cartes=[]):
     """construct all good maps to a big vector
