a7700f20a4e55d88d02edf78b203c4a55a03f6b6
samia88 <samia.aalaou@gmail.com>
2016-02-23 10:46:23 +0100

    Update GenerationClustering.py

--- reseau/mapsClustering/GenerationClustering.py
+++ reseau/mapsClustering/GenerationClustering.py
@@ -5,17 +5,14 @@ from mapsClustering.MapsClustering import MapsClustering
 def GenerationClustering(couche = "conv1", seuilSuppression = 559, seuilBonClustering = 30, fichier = True):
 
     vide_KMNI, pFRJA_R_KMNI, pFRJA_V_KMNI, pFR_RV_KMNI, pCIC_R_KMNI, pCIC_V_KMNI, ind = MapsClustering(couche, seuilSuppression, "kmeansNonInit", fichier)
-    # vide_KMI, pFRJA_R_KMI, pFRJA_V_KMI, pFR_RV_KMI, pCIC_R_KMI, pCIC_V_KMI, ind = MapsClustering(couche, seuilSuppression, "kmeansInit", fichier)
-    # vide_Agglo, pFRJA_R_Agglo, pFRJA_V_Agglo, pFR_RV_Agglo, pCIC_R_Agglo, pCIC_V_Agglo, ind = MapsClustering(couche, seuilSuppression, "agglomerativeClustering", fichier)
+    vide_KMI, pFRJA_R_KMI, pFRJA_V_KMI, pFR_RV_KMI, pCIC_R_KMI, pCIC_V_KMI, indKmeansInit = MapsClustering(couche, seuilSuppression, "kmeansInit", fichier)
     vide_DBSCAN, pFRJA_R_DBSCAN, pFRJA_V_DBSCAN, pFR_RV_DBSCAN, pCIC_R_DBSCAN, pCIC_V_DBSCAN, indDBSCAN = MapsClustering(couche, seuilSuppression, "DBSCAN", fichier)
-    vide_MeanShift, pFRJA_R_MeanShift, pFRJA_V_MeanShift, pFR_RV_MeanShift, pCIC_R_MeanShift, pCIC_V_MeanShift, indMeanShift = MapsClustering(couche, seuilSuppression, "MeanShift", fichier)
+    # vide_MeanShift, pFRJA_R_MeanShift, pFRJA_V_MeanShift, pFR_RV_MeanShift, pCIC_R_MeanShift, pCIC_V_MeanShift, indMeanShift = MapsClustering(couche, seuilSuppression, "MeanShift", fichier)
 
-    print "ind DBSCAN "+ couche + " : ", indDBSCAN
-    print "ind MeanShift "+ couche + " : ", indMeanShift
 
 
     filename = str("../resultats/" + couche + "/cartes_bon_clustering")
-    f = open(filename, "a")
+    f = open(filename, "wb")
 
     ##############################################################################
     #appel directement avec les matrices
@@ -23,72 +20,62 @@ def GenerationClustering(couche = "conv1", seuilSuppression = 559, seuilBonClust
 
     f.write("FRJA R\n")
 
-    clus = bienClusterise(MatriceClustering=pFRJA_R_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI)
+    clus = bienClusterise(MatriceClustering=pFRJA_R_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI, indices=ind[0])
     f.write("kmeansNonInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pFRJA_R_KMI, seuil=seuilBonClustering, listeVide=vide_KMI)
-    # f.write("kmeansInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pFRJA_R_Agglo, seuil=seuilBonClustering, listeVide=vide_Agglo)
-    # f.write("Agglomerative Clustering:" + str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pFRJA_R_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN)
+    clus = bienClusterise(MatriceClustering=pFRJA_R_KMI, seuil=seuilBonClustering, listeVide=vide_KMI, indices=indKmeansInit[0])
+    f.write("kmeansInit:" + str(clus)+"\n")
+    clus = bienClusterise(MatriceClustering=pFRJA_R_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN, indices=indDBSCAN[0])
     f.write("DBSCAN:"+ str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pFRJA_R_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift)
-    f.write("MeanShift:" + str(clus)+"\n")
+    # clus = bienClusterise(MatriceClustering=pFRJA_R_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift, indices= indMeanShift[0])
+    # f.write("MeanShift:" + str(clus)+"\n")
 
 
     f.write("FRJA V\n")
 
-    clus = bienClusterise(MatriceClustering=pFRJA_V_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI)
+    clus = bienClusterise(MatriceClustering=pFRJA_V_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI, indices=ind[1])
     f.write("kmeansNonInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pFRJA_V_KMI, seuil=seuilBonClustering, listeVide=vide_KMI)
-    # f.write("kmeansInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pFRJA_V_Agglo, seuil=seuilBonClustering, listeVide=vide_Agglo)
-    # f.write("Agglomerative Clustering:"+ str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pFRJA_V_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN)
+    clus = bienClusterise(MatriceClustering=pFRJA_V_KMI, seuil=seuilBonClustering, listeVide=vide_KMI, indices=indKmeansInit[1])
+    f.write("kmeansInit:" + str(clus)+"\n")
+    clus = bienClusterise(MatriceClustering=pFRJA_V_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN, indices=indDBSCAN[1])
     f.write("DBSCAN:" + str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pFRJA_V_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift)
-    f.write("MeanShift:" + str(clus)+"\n")
+    # clus = bienClusterise(MatriceClustering=pFRJA_V_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift, indices= indMeanShift[1])
+    # f.write("MeanShift:" + str(clus)+"\n")
 
 
     f.write("FR  RV\n")
 
-    clus = bienClusterise(MatriceClustering=pFR_RV_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI)
+    clus = bienClusterise(MatriceClustering=pFR_RV_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI, indices=ind[2])
     f.write("kmeansNonInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pFR_RV_KMI, seuil=seuilBonClustering, listeVide=vide_KMI)
-    # f.write("kmeansInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pFR_RV_Agglo, seuil=seuilBonClustering, listeVide=vide_Agglo)
-    # f.write("Agglomerative Clustering:" + str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pFR_RV_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN)
+    clus = bienClusterise(MatriceClustering=pFR_RV_KMI, seuil=seuilBonClustering, listeVide=vide_KMI, indices=indKmeansInit[2])
+    f.write("kmeansInit:" + str(clus)+"\n")
+    clus = bienClusterise(MatriceClustering=pFR_RV_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN, indices=indDBSCAN[2])
     f.write("DBSCAN:" + str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pFR_RV_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift)
-    f.write("MeanShift:" + str(clus)+"\n")
+    # clus = bienClusterise(MatriceClustering=pFR_RV_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift, indices= indMeanShift[2])
+    # f.write("MeanShift:" + str(clus)+"\n")
 
 
     f.write("JA correct/incorrect R\n")
 
-    clus = bienClusterise(MatriceClustering=pCIC_R_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI)
+    clus = bienClusterise(MatriceClustering=pCIC_R_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI, indices=ind[3])
     f.write("kmeansNonInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pCIC_R_KMI, seuil=seuilBonClustering, listeVide=vide_KMI)
-    # f.write("kmeansInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pCIC_R_Agglo, seuil=seuilBonClustering, listeVide=vide_Agglo)
-    # f.write("Agglomerative Clustering:" + str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pCIC_R_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN)
+    clus = bienClusterise(MatriceClustering=pCIC_R_KMI, seuil=seuilBonClustering, listeVide=vide_KMI, indices=indKmeansInit[3])
+    f.write("kmeansInit:" + str(clus)+"\n")
+    clus = bienClusterise(MatriceClustering=pCIC_R_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN, indices=indDBSCAN[3])
     f.write("DBSCAN:"+ str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pCIC_R_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift)
-    f.write("MeanShift:" + str(clus)+"\n")
+    # clus = bienClusterise(MatriceClustering=pCIC_R_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift, indices= indMeanShift[3])
+    # f.write("MeanShift:" + str(clus)+"\n")
 
 
     f.write("JA correct/incorrect V\n")
 
-    clus = bienClusterise(MatriceClustering=pCIC_V_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI)
+    clus = bienClusterise(MatriceClustering=pCIC_V_KMNI, seuil=seuilBonClustering, listeVide=vide_KMNI, indices=ind[4])
     f.write("kmeansNonInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pCIC_V_KMI, seuil=seuilBonClustering, listeVide=vide_KMI)
-    # f.write("kmeansInit:" + str(clus)+"\n")
-    # clus = bienClusterise(MatriceClustering=pCIC_V_Agglo, seuil=seuilBonClustering, listeVide=vide_Agglo)
-    # f.write("Agglomerative Clustering:" + str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pCIC_V_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN)
+    clus = bienClusterise(MatriceClustering=pCIC_V_KMI, seuil=seuilBonClustering, listeVide=vide_KMI, indices=indKmeansInit[4])
+    f.write("kmeansInit:" + str(clus)+"\n")
+    clus = bienClusterise(MatriceClustering=pCIC_V_DBSCAN, seuil=seuilBonClustering, listeVide=vide_DBSCAN, indices=indDBSCAN[4])
     f.write("DBSCAN:" + str(clus)+"\n")
-    clus = bienClusterise(MatriceClustering=pCIC_V_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift)
-    f.write("MeanShift:" + str(clus)+"\n")
+    # clus = bienClusterise(MatriceClustering=pCIC_V_MeanShift, seuil=seuilBonClustering, listeVide=vide_MeanShift, indices= indMeanShift[4])
+    # f.write("MeanShift:" + str(clus)+"\n")
 
     f.close()
 
@@ -171,13 +158,3 @@ def GenerationClustering(couche = "conv1", seuilSuppression = 559, seuilBonClust
     #
     #
     # f.close()
-
-
-
-
-
-
-
-
-
-
